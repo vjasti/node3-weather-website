8716628eb45b0f4abeb70c8434fb67a5bd983387
Jasti <vjasti@dxc.com>
2019-09-25 10:59:18 +0530

    updates to heroku app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (lat, lng, callback) => {
-    const url = `https://api.darksky.net/forecast/ff6d3fd1652aab30f9a00aa9c2da8ff8/${lat},${lng}`
+    const url = `https://api.darksky.net/forecast/ff6d3fd1652aab30f9a00aa9c2da8ff8/${lat},${lng}?units=si`
    
     request({url, json: true}, (error, {body}) => {
         if(error) {
@@ -9,7 +9,7 @@ const forecast = (lat, lng, callback) => {
         } else if(body.code) {
             callback(body.error, undefined)
         } else {
-            var summary = `Currently ${body.currently.summary} with ${body.currently.precipProbability*100}% of ${body.currently.precipType}`
+            var summary = `${body.daily.data[0].summary} Currently there is ${body.currently.precipProbability%100}% probability of rain and today's max and min temparatures are ${body.daily.data[0].temperatureHigh}c and ${body.daily.data[0].temperatureLow}c`
             callback(undefined, summary)
         }
     })
